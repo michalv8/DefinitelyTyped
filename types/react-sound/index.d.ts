1424cf707ca009898cfb1e3718acfc75d045ecd9
Michał Tęczyński <michalv8@gmail.com>
2018-10-07 23:07:08 +0200

    Updated react-sound types

--- types/react-sound/index.d.ts
+++ types/react-sound/index.d.ts
@@ -1,34 +1,37 @@
 // Type definitions for react-sound 1.2
 // Project: https://github.com/leoasis/react-sound
 // Definitions by: Konstantin Lebedev <https://github.com/koss-lebedev>
+//                 Michał Tęczyński <https://github.com/michalv8>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import * as React from "react";
 
-declare namespace ReactSound {
-    type PlayStatus = 'PLAYING' | 'STOPPED' | 'PAUSED';
-
-    interface ReactSoundProps {
-        url: string;
-        playStatus: PlayStatus;
-        playFromPosition?: number;
-        position?: number;
-        volume?: number;
-        playbackRate?: number;
-        autoLoad?: boolean;
-        loop?: boolean;
-        onError?: () => void;
-        onLoading?: () => void;
-        onLoad?: () => void;
-        onPlaying?: () => void;
-        onPause?: () => void;
-        onResume?: () => void;
-        onStop?: () => void;
-        onFinishedPlaying?: () => void;
-        onBufferChange?: () => void;
-    }
+export enum PlayStatus {
+    Playing = 'PLAYING',
+    Stopped = 'STOPPED',
+    Paused = 'PAUSED',
 }
 
-declare const ReactSound: React.ComponentClass<ReactSound.ReactSoundProps>;
-export = ReactSound;
+export interface ReactSoundProps {
+    url: string;
+    playStatus: PlayStatus;
+    playFromPosition?: number;
+    position?: number;
+    volume?: number;
+    playbackRate?: number;
+    autoLoad?: boolean;
+    loop?: boolean;
+    onError?: () => void;
+    onLoading?: () => void;
+    onLoad?: () => void;
+    onPlaying?: () => void;
+    onPause?: () => void;
+    onResume?: () => void;
+    onStop?: () => void;
+    onFinishedPlaying?: () => void;
+    onBufferChange?: () => void;
+}
+
+declare class ReactSound extends React.Component<ReactSoundProps> {}
+export default ReactSound;
